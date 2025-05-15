785772719d22b8d46ff68cbc0e7f9393e44f108a
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-05-15 15:27:59 +0700

    fix: change from player.draw() to animate() in startGame function

--- codeWarrior.js
+++ codeWarrior.js
@@ -123,7 +123,7 @@ const movePlayer = (key, xVelocity, isPressed) => {
 const startGame = () => {
     canvas.style.display = 'block';
     startScreen.style.display = 'none';
-    player.draw();
+    animate();
 }
 
 startBtn.addEventListener('click', startGame);
