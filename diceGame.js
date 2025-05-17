41d8304629b6fadd2d7c6caef60882e65184c03f
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-05-17 09:34:20 +0700

    add: declare global variables

---
+++ diceGame.js
@@ -0,0 +1,18 @@
+let listOfAllDice = document.querySelectorAll(".die");
+let scoreInputs = document.querySelectorAll("#score-options input");
+let scoreSpans = document.querySelectorAll("#score-options span");
+let roundElement = document.getElementById("current-round");
+let rollsElement = document.getElementById("current-round-rolls");
+let totalScoreElement = document.getElementById("total-score");
+let scoreHistory = document.getElementById("score-history");
+let rollDiceBtn = document.getElementById("roll-dice-btn");
+let keepScoreBtn = document.getElementById("keep-score-btn");
+let rulesBtn = document.getElementById("rules-btn");
+let rulesContainer = document.querySelector(".rules-container");
+
+let isModalShowing = false;
+let diceValuesArr = [];
+
+let rolls = 0;
+let round = 1;
+let score = 0;
