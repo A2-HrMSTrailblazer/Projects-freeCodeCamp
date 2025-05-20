7168e5c9174e5eb0645826aa79ed4ece6a0081e3
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-05-20 13:22:28 +0700

    add: comment added on top of global variables

--- asychronous.js
+++ asychronous.js
@@ -1,3 +1,4 @@
+// global variables
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
