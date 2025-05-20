5895ed78b5622ee73f85efd34a54261e36bb851e
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-05-20 14:57:11 +0700

    update: td elements updated in showLatestPost function

--- asychronous.js
+++ asychronous.js
@@ -91,10 +91,14 @@ const showLatestPosts = data => {
         return `
             <tr>
                 <td>
-                    <p class="post-title">${title}</p>
+                    <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
                     ${forumCategory(category_id)}
                 </td>
-                <td></td>
+                <td>
+                    <div class="avatar-container">
+                        ${avatars(posters, users)}
+                    </div>
+                </td>
                 <td>${posts_count - 1}</td>
                 <td>${viewCount(views)}</td>
                 <td>${timeAgo(bumped_at)}</td>
