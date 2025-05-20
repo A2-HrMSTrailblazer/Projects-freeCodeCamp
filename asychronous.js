626105d95d668e5be2f91e78c6bd52f1483f31f1
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-05-20 14:34:28 +0700

    update: call forumCategory function in the first table element in showLatestPosts function

--- asychronous.js
+++ asychronous.js
@@ -79,6 +79,7 @@ const showLatestPosts = data => {
             <tr>
                 <td>
                     <p class="post-title">${title}</p>
+                    ${forumCategory(category_id)}
                 </td>
                 <td></td>
                 <td>${posts_count - 1}</td>
