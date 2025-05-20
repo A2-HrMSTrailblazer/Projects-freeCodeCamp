d958d1a99993ad2a97bce249f4ee8819f0464b56
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-05-20 14:31:18 +0700

    add: forumCategory function added

--- asychronous.js
+++ asychronous.js
@@ -17,6 +17,25 @@ const allCategories = {
     560: { category: "Backend Development", className: "backend" },
 };
 
+// function to get the category name and class name
+const forumCategory = id => {
+    let selectedCategory = {};
+    if (allCategories.hasOwnProperty(id)) {
+        const { className, category } = allCategories[id];
+        selectedCategory.className = className;
+        selectedCategory.category = category;
+    }
+    else{
+        selectedCategory.className = "general";
+        selectedCategory.category = "General";
+        selectedCategory.id = 1;
+    }
+    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+    const linkText = selectedCategory.category;
+    const linkClass = `category ${selectedCategory.className}`;
+    return `<a href="${url}" class="${linkClass}" target="_blank">${linkText}</a>`
+};
+
 // time function
 const timeAgo = time => {
     const currentTime = new Date();
